3bcb75a6c9dffb96744f88e9d5bb71947e558b9d
jean-marie <jeanmar-97.1@hotmail.fr>
2022-04-28 13:07:55 +0200

    queryslec

---
+++ app.js
@@ -0,0 +1,43 @@
+var produit = [
+    {
+    nom: 'Blouson Cuir Homme OSX',
+    image: 'https://s1.rockagogostatic.com/ref/pls/pls15/blouson-cuir-mec-marque-osx-brandojacket-pr.jpg',
+    descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, laboriosam.',
+    prix: 225
+    },
+    {
+    nom: 'POLO CINTRE SLIM FIT EN COTON BASIC Bleu',
+    image: 'https://photos6.spartoo.com/photos/188/18830673/18830673_500_A.jpg',
+    descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, laboriosam.',
+        prix: 75
+    },
+    {
+    nom: 'Robe rose croisée à boucler',
+    image: 'https://m1.quebecormedia.com/emp/emp/A1_2_1_d64e884e-d21e-41ab-8eb0-2baf6b656c00_ORIGINAL.jpg?impolicy=cropresize&x=0&y=0&w=802&h=1086&width=925&height=925',
+    descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, laboriosam.',
+    prix: 50
+    },
+    {
+    nom: 'Sneakers Adidas Original Homme',
+    image: 'https://www.kiffoo.com/7220-large_default/basket-adidas-original-homme.jpg',
+    descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, laboriosam.',
+    prix: 159
+    },
+    {
+    nom: 'Pantalon jogging Nike Just Do It - Noir',
+    image: 'https://api.vs.prod.footkorner.nbsaws.com/img/600/744/resize/catalog/product/f/o/footkorner-pantalon-nike-just-do-it-cu4050-010-noir_1_.jpeg',
+    descrption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, laboriosam.',
+    prix: 27
+    }
+];
+    
+/////ouvrir le modal//
+
+var voir_panier_btn_1 = document.querySelector( "#ajout1" )
+var voir_panier_btn_2 = document.querySelector( "#ajout2" )
+var voir_panier_btn_3 = document.querySelector( "#ajout3" )
+var voir_panier_btn_4 = document.querySelector( "#ajout4" )
+var voir_panier_btn_5 = document.querySelector( "#ajout5" )
+var voir_panier_btn_6 = document.querySelector( "#ajout6" )
+
+console.log(voir_panier_btn_1)
